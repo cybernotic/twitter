ed35835b3c3b1a47c9d963c849072608570fd865
Cyber <kwawd@itspace.co.uk>
2022-06-07 13:49:46 +0100

    Create config.php

---
+++ twitter_api/config.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @file
+ * A single location to store configuration.*/
+define('CONSUMER_KEY', '');
+define('CONSUMER_SECRET', '');
+define('ACCESS_TOKEN', '');
+define('ACCESS_TOKEN_SECRET', '');
+?>
